0f17143305f60dc0037032333afe824fd996ba04
toda-keita-git <earth.keita@gmail.com>
2025-10-23 03:17:34 +0900

    変更

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,6 @@ export default defineConfig({
   // },
    preview: {
     host: '0.0.0.0',
-    port: process.env.PORT || 4173,
     allowedHosts: ['learning-frontend-x5jf.onrender.com']
   },
   esbuild: {
